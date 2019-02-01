9a966a664f0f5aab0e8ae03b39291ddf67fa7a2a
FujitaSatoru <fujitasatoru@FujitaSatorunoMacBook-Pro.local>
2019-02-01 15:38:33 +0900

    ruby06 kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   VALID_MAILADDRESS_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, presence: true , format: {with: VALID_MAILADDRESS_REGEX}
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*\d+.*)(?=.*[a-zA-Z]+.*).*\z/
   validates :password, length: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
 
   has_secure_password
